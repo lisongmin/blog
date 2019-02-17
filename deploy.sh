74525263afc4e0effb1643cf4ddb787c2737e60a
lisongmin <lisongmin@126.com>
2019-02-17 16:52:07 +0800

    fix ci failed with 'possibly due to a malformed secure environment variable'

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,19 @@
 
 prepare_ssh()
 {
-    eval $(ssh-agent -s)
-    ssh-add <(echo -e "$blog_key")
-
-    if [ -e /.dockerenv ] ;then
-        mkdir -p ~/.ssh
-        echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
-    fi
+    (npm bin)/set-up-ssh --key "$encrypted_eab015c51e35_key" \
+                         --iv "$encrypted_eab015c51e35_iv" \
+                         --path-encrypted-key ".travis/bot.enc"
 }
 
 deploy() {
+    git config --global user.name lisongmin
+    git config --global user.email lisongmin9@gmail.com
+
     cd public
     git init
     git add .
+    git commit -m "update blog."
     git remote add origin git@github.com:lisongmin/lisongmin.github.io.git
     git push -u origin master --force
 }
